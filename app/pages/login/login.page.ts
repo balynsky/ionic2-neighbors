ff0ddabfdb6202b68cfb4ed9f411c6798fa1893c
Sergiy Balynsky <mail@balynsky.com>
2016-08-20 08:54:01 +0300

    - remove register with google

--- app/pages/login/login.page.ts
+++ app/pages/login/login.page.ts
@@ -82,30 +82,7 @@ export class LoginPage extends BasePage {
             });
         });
     }
-
-    registerUserWithGoogle() {
-        var provider = this.db.getGoogleProvider;
-        this.db.auth.signInWithPopup(provider).then((result)=> {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            // ...
-            LogService.logMessage("Google user ", user);
-        }).catch((error)=> {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-            this.events.publish("user:logout");
-            this.presentToast(this.nav, error);
-        });
-    }
-
+    
     public registerUser(event) {
         this.nav.push(SignupPage);
     }
