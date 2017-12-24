e632a43a5b6a8bf8e230139a536db8daab9d601f
Sergiy Balynsky <mail@balynsky.com>
2017-12-24 15:41:52 +0200

    Change label type on add message

--- src/component/message.modal.ts
+++ src/component/message.modal.ts
@@ -2,51 +2,52 @@ import {ViewController} from "ionic-angular";
 import {Component} from "@angular/core";
 
 @Component({
-    template: `
-<ion-header>
-    <ion-toolbar>
+  template: `
+    <ion-header>
+      <ion-toolbar>
         <ion-title>
-            Сообщение
+          Сообщение
         </ion-title>
         <ion-buttons start>
-            <button ion-button (click)="dismiss()">
-                <span ion-text color="primary" showWhen="ios">Закрыть</span>
-                <ion-icon name="md-close" showWhen="android,windows"></ion-icon>
-            </button>
+          <button ion-button (click)="dismiss()">
+            <span ion-text color="primary" showWhen="ios">Закрыть</span>
+            <ion-icon name="md-close" showWhen="android,windows"></ion-icon>
+          </button>
         </ion-buttons>
         <ion-buttons end>
-            <button ion-button (click)="send()">
-                <span ion-text color="primary" showWhen="ios">Отправить</span>
-                <ion-icon name="md-send" showWhen="android,windows"></ion-icon>
-            </button>
+          <button ion-button (click)="send()">
+            <span ion-text color="primary" showWhen="ios">Отправить</span>
+            <ion-icon name="md-send" showWhen="android,windows"></ion-icon>
+          </button>
         </ion-buttons>
-    </ion-toolbar>
-</ion-header>
+      </ion-toolbar>
+    </ion-header>
 
-<ion-content>
-    <ion-list>
+    <ion-content>
+      <ion-list>
         <ion-item>
-            <ion-textarea rows="5" autosize placeholder="Введите сообщение" clearInput [(ngModel)]="message"></ion-textarea>
+          <ion-label stacked>Введите сообщение</ion-label>
+          <ion-textarea rows="5" autosize clearInput [(ngModel)]="message"></ion-textarea>
         </ion-item>
-    </ion-list>
-</ion-content>
-`
+      </ion-list>
+    </ion-content>
+  `
 })
 export class ModalContentPage {
-    message:string;
+  message: string;
 
-    constructor(public viewCtrl: ViewController) {
-    }
+  constructor(public viewCtrl: ViewController) {
+  }
 
-    dismiss() {
-        this.viewCtrl.dismiss(null);
-    }
+  dismiss() {
+    this.viewCtrl.dismiss(null);
+  }
 
-    send() {
-        if (this.message !== '') {
-            this.viewCtrl.dismiss(this.message);
-        } else {
-            this.dismiss();
-        }
+  send() {
+    if (this.message !== '') {
+      this.viewCtrl.dismiss(this.message);
+    } else {
+      this.dismiss();
     }
+  }
 }
